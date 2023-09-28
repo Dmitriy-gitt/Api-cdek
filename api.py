7a9ddcbeb93dde7ee1074c1e4d43d3fab8559d5b
ehmatthes <eric@example.com>
2023-09-28 10:15:31 +0300

    Commit

--- api.py
+++ api.py
@@ -1,25 +1,32 @@
 import requests
 import map
 
-#получаем токен
+#получаем токен для активации тестового доступа, как указано в документации
+#https://api-docs.cdek.ru/29923918.html
 url = requests.post('https://api.edu.cdek.ru/v2/oauth/token?parameters', data={'grant_type':'client_credentials',
                                                                                'client_id':'EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI',
-                                                                               'client_secret':'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG'})
+                                                                              'client_secret':'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG'})
+#достаем токен из словаря data, для дальнейшего подключения к сервисам
 data = url.json()
 token = data['access_token']
 
 #Авторизация по токену и получаем код города по базе сдека и отправляем данные для карты
 name_city = ['Миасс', 'Златоуст', 'Челябинск', 'Москва']
 for i in name_city:
+    #делаем запрос к списку населенных пунктов,что бы достать код города, как указано в документации
+    #https://api-docs.cdek.ru/33829437.html
     get_city = requests.get(f'https://api.edu.cdek.ru/v2/location/cities?city={i}', headers={'Authorization': 'Bearer ' + token}).json()
+    #достаем код города
     code = get_city[0]['code']
+    #делаем запрос к списку офисов(он же список ПВЗ), как указано в документации
+    #https://api-docs.cdek.ru/36982648.html
     get_adres = requests.get(f'https://api.edu.cdek.ru/v2/deliverypoints?city_code={code}', headers={'Authorization': 'Bearer ' + token}).json()
     for i in get_adres:
-        s_1 = i['location']['address_full']
-        long, lat = i['location']['longitude'], i['location']['latitude']
-        map.search(lat, long, name_city, s_1)
+        s_1 = i['location']['address_full'] #достаем адрес ПВЗ
+        long, lat = i['location']['longitude'], i['location']['latitude'] #достаем долготу и ширину, что бы передать на карту
+        map.search(lat, long, name_city, s_1) #вызов функции из файла map, которая отправляет данные на карту
 
 
-#Получаем список ПВЗ
+#Это можно не читать, моя пометка
 #get_adres = requests.get(f'https://api.edu.cdek.ru/v2/deliverypoints?city_code={code}', headers={'Authorization': 'Bearer ' + token}).json()
 #long, lat = get_adres[0]['location']['longitude'], get_adres[0]['location']['latitude']
--- map.py
+++ map.py
@@ -1,12 +1,14 @@
 import folium
 from folium.plugins import Geocoder
 
+#Создаем карту
 map = folium.Map(location=['55.047874', '60.112206'], zoom_start=8)
 
 d = {}
 
-#Добавляем маркеры на карту
+#Добавляем маркеры с ПВЗ на карту
 def search(lat, long, name_city, s_1):
+    #Передается долгота, ширина, город, адрес
     folium.Marker([lat, long], popup=f'{name_city}', tooltip=s_1).add_to(map)
     map.save('mapcdek.html')
 
